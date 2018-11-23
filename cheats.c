a162308d763e1c28ae88682152e4761b4477b393
TakoEnvironment <45265257+TakoEnvironment@users.noreply.github.com>
2018-11-23 10:30:39 +0900

    Update cheats.c

--- cheats.c
+++ cheats.c
@@ -1,57 +1,25 @@
 #include "cheats.h"
 #include <stdbool.h>
-#include <stdio.h>
 #include "hid.h"
 #include "values.h"
 #include <string.h>
-#include "ID.h"
-#define YOKAI 62
-#define ADDRESS 0x08758FA0
-#define WRITEFLOAT(address, value) *(u32*)(address) += (u32)(value)
+#define WRITEFLOAT_P(address, value) *(u32*)(address) += (u32)(value)
+#define WRITEFLOAT_M(address, value) *(u32*)(address) -= (u32)(value)
+#define SPEED 2  //座標移動のスピード(1の部分)これを#define SPEED 3 みたいに自分好みの速度にする
 
-/**妖怪をボス妖怪にするかなり雑なソースww(失敗作)**/
-void	Boss(void){
-	FILE *tmp_file;
-	static unsigned int tmp = 0;
-	static u32 Y_offset = ADDRESS;
-	static u32 backup[YOKAI] = {0};
-	if(is_pressed(Y+DU)){
-		for(int i = 0;i < YOKAI;i++){
-			if(tmp != 1){
-				tmp_file = fopen("Backup.tk","wb");
-				backup[i] = READU32(Y_offset);
-				fwrite(&backup[i],sizeof(u32),1,tmp_file);
-			}
-			fclose(tmp_file);
-			WRITEU32(Y_offset,BOSS[i]);
-			Y_offset += 0xCC;
+void sample(void){
+	if(is_pressed(Y)){
+		if(is_pressed(DR)){
+			WRITEFLOAT_P(0x0C51D200,SPEED);
 		}
-		tmp = 1;
-		Y_offset = ADDRESS;
-	}
-	if(is_pressed(Y+DD)){
-		for(int i = 0;i < YOKAI;i++){
-			WRITEU32(Y_offset,backup[i]);
-			Y_offset += 0xCC;
+		if(is_pressed(DL)){
+			WRITEFLOAT_M(0x0C51D200,SPEED);
 		}
-		Y_offset = ADDRESS;
+		if(is_pressed(DU)){
+			WRITEFLOAT_P(0x0C51D200,SPEED);
+		}
+		if(is_pressed(DD)){
+			WRITEFLOAT_M(0x0C51D200,SPEED);
+		}
 	}
-}
-
-void	load(void){
-	static u32 read[YOKAI] = {0};
-	static u32 Y_offset = ADDRESS;
-	FILE *file;
-	file = fopen("Backup.tk","rb");
-	for(int i = 0;i < YOKAI;i++){
-		fread(&read[i],sizeof(u32),1,file);
-		WRITEU32(Y_offset,read[i]);
-		Y_offset += 0xCC;
-	}
-	Y_offset = ADDRESS;
-	fclose(file);
-}
-
-void	movement(void){
-	
 }
